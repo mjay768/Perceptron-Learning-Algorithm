fb7f77f1231db42a53f19c43e71117d7b5beb4df
P Manoj Reddy <manoj_ponagandla@hotmail.com>
2019-09-05 16:52:00 -0500

    Add files via upload

--- PLA.py
+++ PLA.py
@@ -1,36 +1,38 @@
 import numpy as np
-data = [[0,5],[2,6],[1,7],
-             [3,-1],[1,-5],[8,9]]
-labels = [0,0,1,1,0,0]
+
 
 print("Hello World!")
-
 class PLA(object):
+    data = [[0, 5], [2, 6], [1, 7],
+            [3, -1], [1, -5], [8, 9]]
+    labels = [0, 0, 1, 1, 0, 0]
 
 
     def __init__(self,epochs,alpha,inputs):
-    
+
         self.epochs = 100
         self.alpha = 0.01
         self.weights = np.zeros(inputs + 1)
-        print("In Init")
+        print("Hi there!")
+        self.train(self.data,self.labels)
 
 
     def predict(self, inputs):
         summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        #print(summation)
         if summation >= 0:
             activation = 1
         else:
             activation = 0
-        print("In Predict")
+
         return activation
 
     def train(self, inputs, true_outputs):
         for _ in range(self.epochs):
             for row, true_value in zip(inputs, true_outputs):
                 prediction = self.predict(row)
-                self.weights[1:] = self.weights[1:] + self.alpha * (true_value - prediction)
+                self.weights[1:] = self.alpha * (true_value - prediction) + self.weights[1:]
                 self.weights[0] = self.alpha * (true_value - prediction)
-                print(self.weights)
+                #print(self.weights)
 
-    object.train(data,labels)
+pla = PLA(epochs=100,alpha=0.01,inputs=2)
